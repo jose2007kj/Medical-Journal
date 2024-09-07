1a83a99ab41c8421103f1ae0a4a3c8a12c2ed214
jose2007kj <jose2007kj@gmail.com>
2024-09-07 23:30:12 +0530

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MedicalJournal.Models;
+namespace MedicalJournal.Data;
+public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MedicalJournal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
